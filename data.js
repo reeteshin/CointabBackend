7489ce91cfff9dbaf5ed32067c49e80e8d8b4931
reetesh meena <reeteshmeena73@gmail.com>
2022-04-25 21:23:59 +0530

    Backend Cointab

---
+++ data.js
@@ -0,0 +1,48 @@
+const rateData = {
+    forward: {
+        a: {
+            first: 29.5,
+            additional: 23.6
+        },
+        b: {
+            first: 33,
+            additional: 28.3
+        },
+        c: {
+            first: 40.1,
+            additional: 38.9
+        },
+        d: {
+            first: 45.4,
+            additional: 44.8
+        },
+        e: {
+            first: 56.6,
+            additional: 55.5
+        },
+    },
+    rto: {
+        a: {
+            first: 43.1,
+            additional: 47.2
+        },
+        b: {
+            first: 53.5,
+            additional: 56.6
+        },
+        c: {
+            first: 72,
+            additional: 77.8
+        },
+        d: {
+            first: 86.7,
+            additional: 89.6
+        },
+        e: {
+            first: 107.3,
+            additional: 111
+        },
+    },
+};
+
+module.exports = rateData;
---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const pincodes = require("./pincode");
+const Compneydata = require("./data")
+ 
+const app = express();
+app.use(express.json());
+ 
+app.get("/:TypeofDelivery/:pin/:WeightinKg", (req, res) => 
+{
+    
+    const pin = `${req.params.pin}`;
+    const WeightinKg = +req.params.WeightinKg;
+    const TypeofDelivery = req.params.TypeofDelivery;
+    if (!validatePin(pin)) 
+    {
+        res.send({ "error": "Invalid PIN : Delivery not available on this pincode" });
+    } 
+    else {
+
+        const zone = validatePin(pin);
+        const cost = findCost(TypeofDelivery, zone, WeightinKg);
+        res.send({ "totalCost": cost });
+    }
+})
+
+function findCost(TypeofDelivery, zone, WeightinKg) {
+    let roundedWeightinKg = Math.round(WeightinKg / 0.5) * 0.5;
+    let multiples = roundedWeightinKg / 0.5;
+    let additionalWeightinKgMultiples = multiples - 1;
+
+    let finalcost1 = Compneydata[TypeofDelivery][zone].first;
+    let finalcost2 = Compneydata[TypeofDelivery][zone].additional;
+
+    if (multiples == 1) {
+        return finalcost1;
+    } else return finalcost1 + (finalcost2 * additionalWeightinKgMultiples);
+}
+
+function validatePin(pin) {
+    let newP = pin.toString();
+    if (pincodes[0][newP]) {
+        return pincodes[0][newP];
+    } else return false;
+}
+
+const Port = process.env.PORT || 1234;
+app.listen(Port, function() {
+    console.log("listening on", Port)
+})
